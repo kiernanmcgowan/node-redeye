7422b47b0dfa096e802b8992d774c4816a59aa8a
The Architect <timmcgowan@syndromesoftware.com>
2012-09-17 11:05:00 -0500

    herp

--- bin/redeye.js
+++ bin/redeye.js
@@ -1,12 +1,17 @@
 (function() {
-  var program, redeye;
 
-  program = require("commander");
-
-  program.version("0.0.3").option("-n, --number <number>", "number of times to convert from js to coffee [1]", Number, 1).option("-p, --path <path>", "root directory path [./]", String, "./").option("-o, --overwrite", "overwrite existing javascript with compiled coffee script").parse(process.argv);
-
-  redeye = require("../lib/redeye");
-
-  redeye(program.path, program.number, program.overwrite);
+  (function() {
+    return (function() {
+      var program, redeye;
+      program = void 0;
+      redeye = void 0;
+      program = void 0;
+      redeye = void 0;
+      program = require("commander");
+      program.version("0.0.3").option("-n, --number <number>", "number of times to convert from js to coffee [1]", Number, 1).option("-p, --path <path>", "root directory path [./]", String, "./").option("-o, --overwrite", "overwrite existing javascript with compiled coffee script").parse(process.argv);
+      redeye = require("../lib/redeye");
+      return redeye(program.path, program.number, program.overwrite);
+    }).call(this);
+  }).call(this);
 
 }).call(this);
