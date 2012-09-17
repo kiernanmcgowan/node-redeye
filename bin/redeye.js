abb4e6309607210cd3932cb48f0b520c9783aab9
The Architect <timmcgowan@syndromesoftware.com>
2012-09-17 10:56:17 -0500

    can now replace your entire code base with redeye!

--- bin/redeye.js
+++ bin/redeye.js
@@ -4,8 +4,9 @@ program
   .version('0.0.1')
   .option('-n, --number <number>', 'number of times to convert from js to coffee [1]', Number, 1)
   .option('-p, --path <path>', 'root directory path [./]', String, './')
+  .option('-o, --overwrite', 'overwrite existing javascript with compiled coffee script')
   .parse(process.argv);
 
 var redeye = require('../lib/redeye');
 
-redeye(program.path, program.number);
+redeye(program.path, program.number, program.overwrite);
