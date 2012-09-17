76fdedf4deedf3e8520a5cb76bed51c513cc7894
The Architect <timmcgowan@syndromesoftware.com>
2012-09-17 12:06:50 -0500

    small fix file io timing

--- bin/redeye.js
+++ bin/redeye.js
@@ -1,7 +1,7 @@
 var program = require('commander');
 
 program
-  .version('0.0.3')
+  .version('0.0.4')
   .option('-n, --number <number>', 'number of times to convert from js to coffee [1]', Number, 1)
   .option('-p, --path <path>', 'root directory path [./]', String, './')
   .option('-o, --overwrite', 'overwrite existing javascript with compiled coffee script')
