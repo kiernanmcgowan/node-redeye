3403d6d4df798f9e3ffc14ddd69106520b256e10
The Architect <timmcgowan@syndromesoftware.com>
2012-09-17 11:04:02 -0500

    one iteration

--- bin/redeye.js
+++ bin/redeye.js
@@ -1,12 +1,12 @@
-var program = require('commander');
+(function() {
+  var program, redeye;
 
-program
-  .version('0.0.3')
-  .option('-n, --number <number>', 'number of times to convert from js to coffee [1]', Number, 1)
-  .option('-p, --path <path>', 'root directory path [./]', String, './')
-  .option('-o, --overwrite', 'overwrite existing javascript with compiled coffee script')
-  .parse(process.argv);
+  program = require("commander");
 
-var redeye = require('../lib/redeye');
+  program.version("0.0.3").option("-n, --number <number>", "number of times to convert from js to coffee [1]", Number, 1).option("-p, --path <path>", "root directory path [./]", String, "./").option("-o, --overwrite", "overwrite existing javascript with compiled coffee script").parse(process.argv);
 
-redeye(program.path, program.number, program.overwrite);
+  redeye = require("../lib/redeye");
+
+  redeye(program.path, program.number, program.overwrite);
+
+}).call(this);
