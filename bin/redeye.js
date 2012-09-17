256ed535ddd4222c97c202f2beaaac37453be9a3
The Architect <timmcgowan@syndromesoftware.com>
2012-09-16 22:47:45 -0500

    second draft

--- bin/redeye.js
+++ bin/redeye.js
@@ -2,13 +2,10 @@ var program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-n, --number', 'number of times to convert from js to coffee [1]', Number, 3000)
-  .option('-p, --path', 'root directory path [./]', String, './')
+  .option('-n, --number <number>', 'number of times to convert from js to coffee [1]', Number, 1)
+  .option('-p, --path <path>', 'root directory path [./]', String, './')
   .parse(process.argv);
 
 var redeye = require('../lib/redeye');
 
-console.log(program.path);
-console.log(program.number);
-
-//redeye(program.p, program.n);
+redeye(program.path, program.number);
